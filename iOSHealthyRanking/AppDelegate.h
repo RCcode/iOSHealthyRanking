f76d33d3921676b8e023c0457aeda0a0060ca822
Liuchunlei1987 <rcplatform@rcplatformdeMac-mini-8.local>
2015-05-13 14:15:39 +0800

    Initial Commit

---
+++ iOSHealthyRanking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSHealthyRanking
+//
+//  Created by TCH on 15/5/13.
+//  Copyright (c) 2015年 com.rcplatform. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSHealthyRanking/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOSHealthyRanking
+//
+//  Created by TCH on 15/5/13.
+//  Copyright (c) 2015年 com.rcplatform. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
